382dc30a4357e232d71d1bc81b551d125be85e55
Asalmerontkd <psrantonio@gmail.com>
2017-01-04 17:44:31 -0600

    Crop add

--- www/js/cropimage.js
+++ www/js/cropimage.js
@@ -1,19 +1,28 @@
 function tomarFoto()
 {
-	navigator.camera.getPicture(onSuccess, onFail, { quality: 25,
+	navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
     destinationType: Camera.DestinationType.FILE_URI,
         encodingType: Camera.EncodingType.JPEG,
         mediaType: Camera.MediaType.PICTURE,
-        allowEdit: true,
         correctOrientation: true
 });
 }
 
 function onSuccess(imageURI) {
-    var image = document.getElementById('myImage');
-    image.src = imageURI;
+	plugins.crop(successCrop, failCrop, imageURI, { quality: 100 });
 }
 
 function onFail(message) {
     alert('Failed because: ' + message);
+}
+
+function successCrop(newPath)
+{
+	var image = document.getElementById('myImage');
+    image.src = newPath;
+}
+
+function failCrop(error)
+{
+	alert('Failed because: ' + error);
 }
